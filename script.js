2d13ddb13e27a86e254639fc453bd36f6289f834
mohapatrabibek9-wq <mohapatrabibek9@gmail.com>
2026-02-13 22:54:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+let currentPage = 1;
+let photoCount = 0;
+let giftCount = 0;
+
+function showPage(n) {
+  document.querySelector('.active').classList.remove('active');
+  document.getElementById('page' + n).classList.add('active');
+  currentPage = n;
+}
+
+function yesClicked() {
+  showPage(2);
+}
+
+function nextPage() {
+  showPage(currentPage + 1);
+}
+
+function noClicked() {
+  document.getElementById('noText').innerText =
+    "WHAT?! 😤 That’s not the right answer… try again 💗";
+}
+
+function photoClicked(el, msg) {
+  if (!el.classList.contains('done')) {
+    el.classList.add('done');
+    photoCount++;
+  }
+  document.getElementById('photoMsg').innerText = msg;
+  if (photoCount === 4) {
+    document.getElementById('photoNext').disabled = false;
+  }
+}
+
+function giftClicked(el, msg) {
+  if (!el.classList.contains('done')) {
+    el.classList.add('done');
+    giftCount++;
+    el.innerText = msg;
+  }
+  if (giftCount === 3) {
+    document.getElementById('giftNext').disabled = false;
+  }
+}
